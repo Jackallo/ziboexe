044ac23dc706109def708d0c6589f09fed9d6ba2
Jackallo <matteo.bonanno91@gmail.com>
2021-06-18 19:34:52 +0200

    Completato es4

--- exercizio/4-pali.js
+++ exercizio/4-pali.js
@@ -1,7 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isPalindrome = void 0;
-function isPalindrome() {
-    console.log("Salve");
+function isPalindrome(text) {
+    var trimmed = textTrimmer(text);
+    for (var i = 0; i < trimmed.length / 2; i++) {
+        var oppositeIndex = trimmed.length - 1 - i;
+        if (trimmed[i] !== trimmed[oppositeIndex])
+            return false;
+    }
+    return true;
 }
 exports.isPalindrome = isPalindrome;
+function textTrimmer(text) {
+    var trimmedText = "";
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] !== " ") {
+            trimmedText += text[i];
+        }
+    }
+    return trimmedText.toLowerCase();
+}
--- exercizio/4-pali.ts
+++ exercizio/4-pali.ts
@@ -1,3 +1,18 @@
-export function isPalindrome() {
-    console.log("Salve")
+export function isPalindrome(text: string): boolean {
+  const trimmed = textTrimmer(text);
+  for (let i = 0; i < trimmed.length / 2; i++) {
+    const oppositeIndex = trimmed.length - 1 - i;
+    if (trimmed[i] !== trimmed[oppositeIndex]) return false;
+  }
+  return true;
+}
+
+function textTrimmer(text: string): string {
+  let trimmedText = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== " ") {
+      trimmedText += text[i];
+    }
+  }
+  return trimmedText.toLowerCase();
 }
